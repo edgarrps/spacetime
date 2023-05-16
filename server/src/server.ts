93abeecd5454dcb06e9c8aa406f529c84e3c29e1
edgarrps <edgarrps@gmail.com>
2023-05-16 15:51:36 -0400

    eslint config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
-import fastify from "fastify"
+import fastify from "fastify";
 
-const app = fastify()
-
-app.get('/hello', () => {
-  return 'Hello World'
-})
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('🚀 HTTP server running on http://localhost:3333')
-})
+const app = fastify();
+app.get("/hello", () => {
+  return "Hello World";
+});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running on http://localhost:3333");
+  });
