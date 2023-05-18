897da12d0c1d6909163197847fcfb915e6751aca
edgarrps <edgarrps@gmail.com>
2023-05-17 23:03:27 -0400

    connection with db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
-app.get("/hello", () => {
-  return "Hello World";
+const prisma = new PrismaClient();
+
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+
+  return users;
 });
+
 app
   .listen({
     port: 3333,
