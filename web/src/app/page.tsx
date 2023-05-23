ca4e9483fea184925bd7751d6689634cd2a800c6
edgarrps <edgarrps@gmail.com>
2023-05-22 21:47:44 -0400

    mobile project

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-zync-50 h-screen bg-zinc-950">
-      <h1>Sua cápsula do tempo</h1>
+    <div className="text-zync-50 h-screen bg-zinc-950 p-6">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )
 }
