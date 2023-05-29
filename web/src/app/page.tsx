eadb6d4418a092ba23b766d12b03ee52029f1474
edgarrps <edgarrps@gmail.com>
2023-05-28 22:23:45 -0400

    gray color

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-zync-50 h-screen bg-zinc-950 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold text-white">Sua cápsula do tempo</h1>
     </div>
   )
 }
