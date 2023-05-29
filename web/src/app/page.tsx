078f49b7d44e48193fa704f1483d3260dd7f99c3
Edgar Santos <68236516+edgarrps@users.noreply.github.com>
2023-05-29 16:26:57 -0400

    Update page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div></div>
+      <div className="flex flex-col p-16">
+        <p>Local do spacetime</p>
+      </div>
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
