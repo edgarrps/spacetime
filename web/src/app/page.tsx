3002f6f5d9a91acdcef5f57f69146145081f8fef
edgarrps <edgarrps@gmail.com>
2023-05-31 21:15:38 -0400

    border center

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-cyan-700 opacity-50 blur-full"></div>
-        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes"></div>
+        <div className="absolute bottom-0 right-0 right-1 top-0 w-2 bg-stripes"></div>
       </div>
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
