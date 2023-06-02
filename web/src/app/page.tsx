3b3f155754b773adda1f90303a092e8948a32431
edgarrps <edgarrps@gmail.com>
2023-06-02 19:07:02 -0400

    user button

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,16 @@
+import { User } from 'lucide-react'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-cyan-700 opacity-50 blur-full"></div>
         <div className="absolute bottom-0 right-0 right-1 top-0 w-2 bg-stripes"></div>
+        <a href="" className="flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+        </a>
       </div>
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
