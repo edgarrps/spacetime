7f5264ea783b7537ce7b81cde43bdeeaf96e78ff
edgarrps <edgarrps@gmail.com>
2023-06-17 19:35:09 -0400

    mobile fonts imported

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className="flex-1 bg-zync-950">
+    <View className="bg-zync-950 flex-1">
       <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
