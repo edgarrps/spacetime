52d451c5d6c8f99cd60c3c3dc2c84d190dfbe2dc
edgarrps <edgarrps@gmail.com>
2023-07-17 23:06:13 -0400

    stripes added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,7 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -27,6 +28,7 @@ export default function App() {
       className="relative flex-1 bg-gray-900"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <Stripes />
       <Text>Test</Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
